d7f5977fd15dca6a95080655cbe16d527d9a6668
Nate <nwilliam@users.noreply.github.com>
2026-01-27 08:56:36 -0600

    Added tooltip helper and started adding tooltips and cleaning up the breakdown table.

--- js/app.js
+++ js/app.js
@@ -37,6 +37,15 @@ function renderAirportList(listEl, airports) {
   listEl.hidden = airports.length === 0;
 }
 
+function tooltip(text, iconPath = "assets/icons/question-mark.svg") {
+  return `
+    <span class="tooltip-icon">
+      <img alt="tooltip" src="${iconPath}" />
+      <span class="tooltip-text">${text}</span>
+    </span>
+  `;
+}
+
 let debounceTimer = null;
 destInput.addEventListener("input", () => {
   selectedAirport1 = null;
@@ -74,7 +83,6 @@ function scheduleRecalc() {
 inputs.forEach(input => {
   input.addEventListener("input", scheduleRecalc);
 });
-
 
 // Calculation Logic
 function calculateAndRender() {
@@ -147,115 +155,133 @@ function calculateAndRender() {
   document.getElementById("fly-total-kodiak").textContent = "$" + flyTotalKodiak.toLocaleString(undefined, {maximumFractionDigits: 0});
 
   // --- Breakdown Table ---
-  const breakdownTable = document.getElementById("breakdownTable");
-  breakdownTable.innerHTML = `
-<tr><th colspan="3" style="text-align:center">Employee Costs (Travel Hours Only, Driving)</th></tr>
-<tr>
-  <td>${ROLES.directors.label}
-    <span class="tooltip-icon">
-      <img alt="tooltip" src="assets/icons/question-mark.svg"/>
-      <span class="tooltip-text">Average Director/P.E. cost per hour is $${ROLES.directors.hourlyRate.toFixed(2)}, includes the cost of benefits. MnDOT and PRC Aviation have determined that the value these individuals create is worth a factor of ${ROLES.directors.prcFactor} times their salary. Thus, we multiply hourly cost times the PRC factor to calculate the cost per hour of having this individual driving a vehicle rather than working. ("Windshield Time")</span>
-    </span>
-  </td>
-  <td>${ROLES.directors.hourlyRate.toFixed(2)}/hr cost x ${ROLES.directors.prcFactor} PRC Factor x ${numDirectors} ${ROLES.directors.label} traveling</td>
-  <td>$${costDirectors.toLocaleString(undefined, {maximumFractionDigits: 2})}/hr</td>
-</tr>
-<tr>
-  <td>Managers
-    <span class="tooltip-icon">
-      <img alt="tooltip" src="assets/icons/question-mark.svg"/>
-      <span class="tooltip-text">Average Manager/Supervisor/Professional cost per hour is $${ROLES.managers.hourlyRate.toFixed(2)}, includes the cost of benefits. MnDOT and PRC Aviation have determined that the value these individuals create is worth a factor of ${ROLES.managers.prcFactor} their salary. Thus, we multiply hourly cost times the PRC factor to calculate the cost per hour of having this individual driving a vehicle rather than working. ("Windshield Time")</span>
-    </span>
-  </td>
-  <td>${ROLES.managers.hourlyRate.toFixed(2)}/hr cost x ${ROLES.managers.prcFactor} PRC Factor x ${numManagers} managers traveling</td>
-  <td>$${costManagers.toLocaleString(undefined, {maximumFractionDigits: 2})}/hr</td>
-</tr>
-<tr>
-  <td>Generalists
-    <span class="tooltip-icon">
-      <img alt="tooltip" src="assets/icons/question-mark.svg"/>
-      <span class="tooltip-text">Average Generalist cost per hour is $${ROLES.generalists.hourlyRate.toFixed(2)}, includes the cost of benefits. MnDOT and PRC Aviation have determined that the value these individuals create is worth a factor of ${ROLES.generalists.prcFactor} their salary. Thus, we multiply hourly cost times the PRC factor to calculate the cost per hour of having this individual driving a vehicle rather than working. ("Windshield Time")</span>
-    </span>
-  </td>
-  <td>${ROLES.generalists.hourlyRate.toFixed(2)}/hr cost x ${ROLES.generalists.prcFactor} PRC Factor x ${numGeneralists} generalists traveling</td>
-  <td>$${costGeneralists.toLocaleString(undefined, {maximumFractionDigits: 2})}/hr</td>
-</tr>
-<tr class="total-row">
-  <td><b>Employee Total</b></td><td></td>
-  <td><b>$${totalEmployeeCostPerHour.toLocaleString(undefined, {maximumFractionDigits: 2})}/hr</b></td>
-</tr>
+    const breakdownTable = document.getElementById("breakdownTable");
+    breakdownTable.innerHTML = `
+  <tr><th colspan="3" style="text-align:center">Employee Costs (Travel Hours Only, Driving)</th></tr>
+  <tr>
+    <td>${ROLES.directors.shortLabel}
+        ${tooltip(`Average Director/P.E. cost per hour is $${ROLES.directors.hourlyRate.toFixed(2)},
+        includes the cost of benefits. MnDOT and PRC Aviation have determined that the value these individuals create " +
+        is worth a factor of $${ROLES.directors.prcFactor} times their salary. Thus, we multiply hourly cost times the
+        PRC factor to calculate the cost per hour of having this individual driving a vehicle rather than working.
+        ("Windshield Time")`)}
+    <td>${ROLES.directors.hourlyRate.toFixed(2)}/hr cost x ${ROLES.directors.prcFactor} PRC Factor x ${numDirectors} ${ROLES.directors.shortLabel.toLowerCase()} traveling</td>
+    <td>$${costDirectors.toLocaleString(undefined, {maximumFractionDigits: 2})}/hr</td>
+  </tr>
+  <tr>
+    <td>${ROLES.managers.shortLabel}
+      ${tooltip(`Average Manager/Supervisor/Professional cost per hour is
+      $${ROLES.managers.hourlyRate.toFixed(2)}, includes the cost of benefits. MnDOT and PRC Aviation have
+      determined that the value these individuals create is worth a factor of ${ROLES.managers.prcFactor} their salary.
+      Thus, we multiply hourly cost times the PRC factor to calculate the cost per hour of having this individual
+      driving a vehicle rather than working. ("Windshield Time")`)}
+    </td>
+    <td>${ROLES.managers.hourlyRate.toFixed(2)}/hr cost x ${ROLES.managers.prcFactor} PRC Factor x ${numManagers} ${ROLES.managers.shortLabel.toLowerCase()} traveling</td>
+    <td>$${costManagers.toLocaleString(undefined, {maximumFractionDigits: 2})}/hr</td>
+  </tr>
+  <tr>
+    <td>${ROLES.generalists.shortLabel}
+      ${tooltip(`Average Generalist cost per hour is $${ROLES.generalists.hourlyRate.toFixed(2)},
+       includes the cost of benefits. MnDOT and PRC Aviation have determined that the value these individuals create
+       is worth a factor of ${ROLES.generalists.prcFactor} their salary. Thus, we multiply hourly cost times the PRC
+       factor to calculate the cost per hour of having this individual driving a vehicle rather than working.
+       ("Windshield Time")`)}
+    </td>
+    <td>${ROLES.generalists.hourlyRate.toFixed(2)}/hr cost x ${ROLES.generalists.prcFactor} PRC Factor x ${numGeneralists} ${ROLES.generalists.shortLabel.toLowerCase()} traveling</td>
+    <td>$${costGeneralists.toLocaleString(undefined, {maximumFractionDigits: 2})}/hr</td>
+  </tr>
+  <tr class="total-row">
+    <td><b>Employee Total</b>
+        <!--${tooltip(`Total cost of all employees traveling, per hour.`)}--></td><td></td>
+    <td><b>$${totalEmployeeCostPerHour.toLocaleString(undefined, {maximumFractionDigits: 2})}/hr</b></td>
+  </tr>
 
-<tr><th colspan="3" style="text-align:center">Driving Costs</th></tr>
-<tr>
-  <td>Travel Hours</td>
-  <td>${selectedAirport1.drivingFromKSTP} miles each way ÷ ${DRIVING_SPEED_MPH} mph</td>
-  <td>${driveHours.toFixed(2)} hrs</td>
-</tr>
-<tr>
-  <td>Employee Cost</td>
-  <td>Total employee cost per hour * travel hours</td>
-  <td>$${driveEmployeeTotal.toLocaleString(undefined, {maximumFractionDigits: 0})}</td>
-</tr>
-<tr>
-  <td>Cars Needed</td>
-  <td>Total Employees: ${totalEmployees} ÷ ${VEHICLE_CAPACITY} per car</td>
-  <td>${carsNeeded}</td>
-</tr>
-<tr>
-  <td>Distance Cost</td>
-  <td>${selectedAirport1.drivingFromKSTP} mi * $${COST_PER_MILE.driving} * ${ROUND_TRIP ? 2 : 1} * ${carsNeeded} cars</td>
-  <td>$${driveDistanceCost.toLocaleString(undefined, {maximumFractionDigits: 0})}</td>
-</tr>
-<tr>
-  <td>Lodging & Meals</td>
-  <td>+$${LODGING_PER_PERSON} per person if travel plus destination hours > ${HOURS_ALLOWED_PER_DAY}</td>
-  <td>$${driveLodging.toLocaleString()}</td>
-</tr>
-<tr class="total-row">
-  <td colspan="2"><b>Total Driving</b></td>
-  <td><b>$${driveTotal.toLocaleString(undefined, {maximumFractionDigits: 0})}</b></td>
-</tr>
+  <tr><th colspan="3" style="text-align:center">Driving Costs</th></tr>
+  <tr>
+    <td>Travel Hours
+        ${tooltip(`Average travel time from KSTP to ${selectedAirport1.name} airport, in hours. Mileage is derived using
+        MapQuest API to calculate actual driving distances with an average driving speed of ${DRIVING_SPEED_MPH} mph.`)}
+    </td>
+    <td>${selectedAirport1.drivingFromKSTP * (ROUND_TRIP ? 2 : 1)} ${ROUND_TRIP ? `total round-trip miles` : `miles`} ÷ ${DRIVING_SPEED_MPH} mph</td>
+    <td>${driveHours.toFixed(2)} hrs</td>
+  </tr>
+  <tr>
+    <td>Employee Cost
+        ${tooltip(`Employee Costs in hours from above multiplied by the number of hours they are traveling. This
+        represents their "windshield time" where they are unable to perform their duties for MnDOT. Time spent in a
+        car is considered dead time because the environment is not conducive to work.`)}</td>
+    <td>Total employee cost per hour x hours traveling</td>
+    <td>$${driveEmployeeTotal.toLocaleString(undefined, {maximumFractionDigits: 0})}</td>
+  </tr>
+  <tr>
+    <td>Vehicles Needed
+        ${tooltip(`Number of vehicles needed to transport employees. We assume ${VEHICLE_CAPACITY} employees traveling per vehicle.
+        This is used to determine the total mileage which must either be reimbursed or the cost to MnDOT for a MnDOT vehicle.`)}</td>
+    <td>${totalEmployees} employees traveling ÷ ${VEHICLE_CAPACITY} per vehicle</td>
+    <td>${carsNeeded}</td>
+  </tr>
+  <tr>
+    <td>Distance Cost
+        ${tooltip(`Total mileage cost of driving to the destination ${ROUND_TRIP ? "and back" : ""} multiplied by
+        the current federal mileage reimbursement rate of $${COST_PER_MILE.driving}, then multiplied by the number of vehicles
+        traveling.`)}</td>
+    <td>${selectedAirport1.drivingFromKSTP * (ROUND_TRIP ? 2 : 1)} ${ROUND_TRIP ? `total round-trip miles` : `miles`} x
+        $${COST_PER_MILE.driving} Federal mileage reimbursement rate x ${carsNeeded} vehicles needed</td>
+    <td>$${driveDistanceCost.toLocaleString(undefined, {maximumFractionDigits: 0})}</td>
+  </tr>
+  <tr>
+    <td>Lodging & Meals
+        ${tooltip(`If the total hours is greater than a standard workday of ${HOURS_ALLOWED_PER_DAY} hours,
+        employees are reimbursed for an overnight stay including lodging and meals. This cost is ${LODGING_PER_PERSON} per person
+        and is based on $120 lodging cost please three meals reimbursed at $11, $13, and $19 dollars.`)}</td>
+    <td>+$${LODGING_PER_PERSON} per person if travel plus destination hours is greater than ${HOURS_ALLOWED_PER_DAY}</td>
+    <td>$${driveLodging.toLocaleString()}</td>
+  </tr>
+  <tr class="total-row">
+    <td colspan="2"><b>Total Driving</b></td>
+    <td><b>$${driveTotal.toLocaleString(undefined, {maximumFractionDigits: 0})}</b></td>
+  </tr>
 
-<tr><th colspan="3" style="text-align:center">Flying Costs - King Air (Employee Hours NOT included)</th></tr>
-<tr>
-  <td>Travel Hours</td>
-  <td>${selectedAirport1.flyingFromKSTP} miles each way ÷ ${FLYING_SPEED_MPH.kingAir} mph</td>
-  <td>${flyHoursKingAir.toFixed(2)} hrs</td>
-</tr>
-<tr>
-  <td>Distance Cost</td>
-  <td>${selectedAirport1.flyingFromKSTP} mi * $${COST_PER_MILE.flyingKingAir} * ${ROUND_TRIP ? 2 : 1}</td>
-  <td>$${flyDistanceCostKingAir.toLocaleString(undefined, {maximumFractionDigits: 0})}</td>
-</tr>
-<tr>
-  <td>Lodging & Meals</td>
-  <td>+$${LODGING_PER_PERSON} per person including two pilots + $${PILOT_LODGING} pilot lodging if travel hours plus destination hours > ${HOURS_ALLOWED_PER_DAY_FLYING}</td>
-  <td>$${flyLodgingKingAir.toLocaleString()}</td>
-</tr>
-<tr class="total-row">
-  <td colspan="2"><b>Total Flying</b></td>
-  <td><b>$${flyTotalKingAir.toLocaleString(undefined, {maximumFractionDigits: 0})}</b></td>
-</tr>
+  <tr><th colspan="3" style="text-align:center">Flying Costs - King Air (Employee Hours NOT included)</th></tr>
+  <tr>
+    <td>Travel Hours</td>
+    <td>${selectedAirport1.flyingFromKSTP} miles each way ÷ ${FLYING_SPEED_MPH.kingAir} mph</td>
+    <td>${flyHoursKingAir.toFixed(2)} hrs</td>
+  </tr>
+  <tr>
+    <td>Distance Cost</td>
+    <td>${selectedAirport1.flyingFromKSTP} mi * $${COST_PER_MILE.flyingKingAir} * ${ROUND_TRIP ? 2 : 1}</td>
+    <td>$${flyDistanceCostKingAir.toLocaleString(undefined, {maximumFractionDigits: 0})}</td>
+  </tr>
+  <tr>
+    <td>Lodging & Meals</td>
+    <td>+$${LODGING_PER_PERSON} per person including two pilots + $${PILOT_LODGING} pilot lodging if travel hours plus destination hours > ${HOURS_ALLOWED_PER_DAY_FLYING}</td>
+    <td>$${flyLodgingKingAir.toLocaleString()}</td>
+  </tr>
+  <tr class="total-row">
+    <td colspan="2"><b>Total Flying</b></td>
+    <td><b>$${flyTotalKingAir.toLocaleString(undefined, {maximumFractionDigits: 0})}</b></td>
+  </tr>
 
-<tr><th colspan="3" style="text-align:center">Flying Costs - Kodiak (Employee Hours NOT included)</th></tr>
-<tr>
-  <td>Travel Hours</td>
-  <td>${selectedAirport1.flyingFromKSTP} miles each way ÷ ${FLYING_SPEED_MPH.kodiak} mph</td>
-  <td>${flyHoursKodiak.toFixed(2)} hrs</td>
-</tr>
-<tr>
-  <td>Distance Cost</td>
-  <td>${selectedAirport1.flyingFromKSTP} mi * $${COST_PER_MILE.flyingKodiak} * ${ROUND_TRIP ? 2 : 1}</td>
-  <td>$${flyDistanceCostKodiak.toLocaleString(undefined, {maximumFractionDigits: 0})}</td>
-</tr>
-<tr>
-  <td>Lodging & Meals</td>
-  <td>+$${LODGING_PER_PERSON} per person including two pilots + $${PILOT_LODGING} pilot lodging if travel hours plus destination hours > ${HOURS_ALLOWED_PER_DAY_FLYING}</td>
-  <td>$${flyLodgingKodiak.toLocaleString()}</td>
-</tr>
-<tr class="total-row">
-  <td colspan="2"><b>Total Flying</b></td>
-  <td><b>$${flyTotalKodiak.toLocaleString(undefined, {maximumFractionDigits: 0})}</b></td>
-</tr>
-  `;
-}
+  <tr><th colspan="3" style="text-align:center">Flying Costs - Kodiak (Employee Hours NOT included)</th></tr>
+  <tr>
+    <td>Travel Hours</td>
+    <td>${selectedAirport1.flyingFromKSTP} miles each way ÷ ${FLYING_SPEED_MPH.kodiak} mph</td>
+    <td>${flyHoursKodiak.toFixed(2)} hrs</td>
+  </tr>
+  <tr>
+    <td>Distance Cost</td>
+    <td>${selectedAirport1.flyingFromKSTP} mi * $${COST_PER_MILE.flyingKodiak} * ${ROUND_TRIP ? 2 : 1}</td>
+    <td>$${flyDistanceCostKodiak.toLocaleString(undefined, {maximumFractionDigits: 0})}</td>
+  </tr>
+  <tr>
+    <td>Lodging & Meals</td>
+    <td>+$${LODGING_PER_PERSON} per person including two pilots + $${PILOT_LODGING} pilot lodging if travel hours plus destination hours > ${HOURS_ALLOWED_PER_DAY_FLYING}</td>
+    <td>$${flyLodgingKodiak.toLocaleString()}</td>
+  </tr>
+  <tr class="total-row">
+    <td colspan="2"><b>Total Flying</b></td>
+    <td><b>$${flyTotalKodiak.toLocaleString(undefined, {maximumFractionDigits: 0})}</b></td>
+  </tr>
+    `;
+  }
--- js/constants.js
+++ js/constants.js
@@ -1,18 +1,18 @@
 const ROLES = {
   directors: {
-    label: "Directors",
+    shortLabel: "Directors",
     fullLabel: "Office Directors/Principle Engineers",
     hourlyRate: 86.53,
     prcFactor: 5.7
   },
   managers: {
-    label: "Managers",
+    shortLabel: "Managers",
     fullLabel: "Managers/Supervisors/Professionals",
     hourlyRate: 67.46,
     prcFactor: 3.8
   },
   generalists: {
-    label: "Generalists",
+    shortLabel: "Generalists",
     fullLabel: "Generalists/Transportation Worker",
     hourlyRate: 51.40,
     prcFactor: 2.4
@@ -31,7 +31,7 @@ const FLYING_SPEED_MPH = {
 };
 
 const VEHICLE_CAPACITY = 4;
-const LODGING_PER_PERSON = 150; //includes meals? No way to figure out TOD so lets say hotel = 120 + (11 + 13 + 19)/3 = 11 so 22-ish? So 142? Maybe 150?
+const LODGING_PER_PERSON = 163; //includes meals? No way to figure out TOD so lets say hotel = 120 + (11 + 13 + 19) = 163
 const PILOT_LODGING = 272; // Why are we doing this separately? Maybe we should just use the same rate as generalists?
 const DRIVING_SPEED_MPH = 60;
 const HOURS_ALLOWED_PER_DAY = 10;
